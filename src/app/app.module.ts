d8acc1aeeb73f66b255098e7dc9658fa0352834e
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-10-19 14:57:38 -0700

    import footer component class and declare it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { MainContentBodyComponent } from './main-content-body/main-content-body.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationBarComponent,
-    MainContentBodyComponent
+    MainContentBodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
