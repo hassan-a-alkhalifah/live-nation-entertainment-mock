776bf9c49bb8b3adb7218ce87c5806592e44ea9c
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-10-26 11:33:16 -0700

    retieve image object from firebase and selector and store footer logo urls in relevant variables

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { ImageService } from '../image.service';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
+  providers: [ImageService]
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  images : FirebaseListObservable<any[]>;
+  liveNationLogoFooter: string;
+  ticketmasterLogoFooter: string;
+  liveNationMediaAndSponsorshipsLogoFooter: string;
+  houseOfBluesLogoFooter: string;
+  liveNationProductionsLogoFooter: string;
+
+  constructor(private imageService: ImageService) { }
 
   ngOnInit() {
+    this.images = this.imageService.getImages();
+    this.images.subscribe(p => {
+      this.liveNationLogoFooter = p[7].liveNationLogoFooter;
+      this.ticketmasterLogoFooter = p[8].ticketmasterLogoFooter;
+      this.liveNationMediaAndSponsorshipsLogoFooter = p[9].liveNationMediaAndSponsorshipsLogoFooter;
+      this.houseOfBluesLogoFooter = p[10].houseOfBluesLogoFooter;
+      this.liveNationProductionsLogoFooter = p[11].liveNationProductionsLogoFooter;
+    });
   }
 
 }
