fc7da8595647be5091a531711bf23fd91c781996
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-10-26 08:45:48 -0700

    import LandingPagecomponent class and add to appRoutes array

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,12 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 const appRoutes: Routes = [
-  // {
-  //   path: '',
-  //   component: componentName
-  // }
+  {
+    path: '',
+    component: LandingPageComponent
+  }
 ]
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
