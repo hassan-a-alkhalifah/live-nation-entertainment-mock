956d094210d987e6ea9baed40ad63c96f7fc528a
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-10-26 11:19:04 -0700

    retieve image object from firebase and selector and store concert links logo urls in relevant variables

--- src/app/main-content-body/main-content-body.component.ts
+++ src/app/main-content-body/main-content-body.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { ImageService } from '../image.service';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-main-content-body',
   templateUrl: './main-content-body.component.html',
-  styleUrls: ['./main-content-body.component.css']
+  styleUrls: ['./main-content-body.component.css'],
+  providers: [ImageService]
 })
 export class MainContentBodyComponent implements OnInit {
 
-  constructor() { }
+  images : FirebaseListObservable<any[]>;
+  userProfileIcon: string;
+  globeIcon: string;
+  liveNationConcertsLogoGrey: string;
+  liveNationMediaAndSponsorshipsLogoGrey: string;
+  ticketmasterLogoGrey: string;
+  liveNationEntertainmentLogoGrey: string;
+
+
+  constructor(private imageService: ImageService) { }
 
   ngOnInit() {
+    this.images = this.imageService.getImages();
+    this.images.subscribe(p => {
+      this.userProfileIcon = p[1].userProfileIcon;
+      this.globeIcon = p[2].globeIcon;
+      this.liveNationConcertsLogoGrey = p[3].liveNationConcertsLogoGrey;
+      this.liveNationMediaAndSponsorshipsLogoGrey = p[4].liveNationMediaAndSponsorshipsLogoGrey;
+      this.ticketmasterLogoGrey = p[5].ticketmasterLogoGrey;
+      this.liveNationEntertainmentLogoGrey = p[6].liveNationEntertainmentLogoGrey;
+    });
   }
 
 }
