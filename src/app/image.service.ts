ae013cbcec8735b2fa0d00340f31daf031d41073
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-10-26 10:41:04 -0700

    correct main logo url

--- src/app/image.service.ts
+++ src/app/image.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Injectable()
 export class ImageService {
+  images: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private database: AngularFireDatabase) {
+    this.images = database.list('images');
+  }
 
+  getImages() {
+    return this.images;
+  }
 }
