6883f137a042b8a52fdfe177c6050961b15b9090
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-10-26 09:41:30 -0700

    add firebase credentials

---
+++ src/app/api-key.ts
@@ -0,0 +1,7 @@
+export const masterFirebaseConfig = {
+    apiKey: "AIzaSyA1ksqQ35zMz-rS4p5IKhaO9DSQkny7pqs",
+    authDomain: "live-nation-entertainment-mock.firebaseapp.com",
+    databaseURL: "https://live-nation-entertainment-mock.firebaseio.com",
+    storageBucket: "live-nation-entertainment-mock.appspot.com",
+    messagingSenderId: "571419759063"
+  };
